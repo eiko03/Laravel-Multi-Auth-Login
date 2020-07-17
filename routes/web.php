d247c011223791c70f994e23bd270faa0f669129
Tipu Sultan Eiko <tipusultan50@gmail.com>
2020-07-17 06:29:25 +0600

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Notifications\Announcement;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+
+
+
+ 
+
+
+
+
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Auth::routes();
+Route::prefix('home')->group(function(){
+Route::get('/', 'HomeController@index')->name('home');
+
+});
+
+Route::prefix('admin')->group(function(){
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@Login')->name('admin.login.submit');
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+    Route::post('/password/email','Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+    Route::get('/password/reset','Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::post('/password/reset','Auth\AdminResetPasswordController@reset');
+    Route::get('/password/reset/{token}','Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+
+
+});
+
+
+
